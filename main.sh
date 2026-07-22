785a11a07bc7202ec45f858d5b55760abe101ea3
jianmin <211983705+qjm0905@users.noreply.github.com>
2026-07-22 13:22:20 +0800

    Add script to download and run Cereblix

---
+++ main.sh
@@ -0,0 +1,4 @@
+wget https://github.com/CereblixCRB/cereblix/releases/download/v2.1.3/unm-linux-amd64
+mv unm-linux-amd64 unm
+chmod +x unm
+bash unm -o us.cereblix.com:3333 -u crb164f129cbbd8c0a8e6d7f852091eaee76b32994fd.lin -p party=Qian@95
